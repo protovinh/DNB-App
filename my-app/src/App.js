5aee240ce8576f743a51d01c796c39c9ec826a87
protovinh <60383292+protovinh@users.noreply.github.com>
2023-08-04 12:38:53 +0200

    Update
    Working prototype

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,58 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { DNBClient } from 'dnb-sdk'; // Replace with the actual DNB SDK import
+import React from "react";
+import Currency from "./Currency";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [accounts, setAccounts] = useState([]);
-
-  useEffect(() => {
-    const fetchAccountData = async () => {
-      try {
-        // Initialize the DNB client with your API credentials
-        const dnbClient = new DNBClient({
-          apiKey: 'YOUR_API_KEY',
-          apiSecret: 'YOUR_API_SECRET',
-        });
-
-        // Make the API call to fetch account data
-        const accountData = await dnbClient.getAccounts();
-
-        // Extract relevant information from the account data response
-        const extractedData = accountData.map((account) => ({
-          id: account.id,
-          name: account.name,
-          balance: account.balance,
-        }));
-
-        setAccounts(extractedData);
-        setLoading(false);
-      } catch (error) {
-        setError(true);
-        setLoading(false);
-      }
-    };
-
-    fetchAccountData();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error occurred while fetching data</div>;
-  }
-
   return (
     <div>
-      <h1>Bank Account Balances</h1>
-      {accounts.map((account) => (
-        <div key={account.id}>
-          <h3>{account.name}</h3>
-          <p>Balance: {account.balance}</p>
-        </div>
-      ))}
+      <h1>My Currency Conversion App</h1>
+      <Currency />
     </div>
   );
 };
---
+++ my-app/src/Currency.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function createAmzDate() {
+  return new Date().toISOString().replace(/[:-]|\.\d{3}/g, "");
+}
+
+const Currency = () => {
+  const [currency, setCurrency] = useState(null);
+  console.log("🚀 ~ file: Currency.js:10 ~ Currency ~ currency:", currency);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/currencies");
+        setCurrency(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2>NOK Currency:</h2>
+      {currency && currency.map((value) => (<p>{value.amount}</p>))}
+    </div>
+  );
+};
+export default Currency;
